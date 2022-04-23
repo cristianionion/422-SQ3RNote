d0f82d842efa9a6477c5019ab85e8f11477bd00a
tiana cook <tianacook11@yahoo.com>
2022-04-23 13:36:33 -0700

    tiana added comments

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+
+# import the sql driver
 import mysql.connector
 
 conn = mysql.connector.connect(host="localhost", port=3306, user="root", passwd="")
 
+# create the database if it does not exist
 def db_create_db(conn):
     mycursor = conn.cursor()
     mycursor.execute("CREATE DATABASE IF NOT EXISTS db_notes")
 
+
 db_create_db(conn)
 
 
-#
+# create the database tables
+# which will include, book title, chapter title, notes, and questions
+
 def db_create_table(conn, book):
     db_create_db(conn)
     conn.database = "db_notes"
@@ -21,6 +27,8 @@ def db_create_table(conn, book):
 bookinput = "book20"
 db_create_table(conn, bookinput)
 
+
+# insert note function so that we can insert data into the database
 
 def db_insert_note(conn,table, title,chapter,notes, question):
     conn.database = "db_notes"
@@ -48,7 +56,7 @@ def db_update_all(conn,table, notes, question, chapter):
 db_update_all(conn,bookinput, "fake notes" ,"fake question","c")
 
 
-
+# function to retrieve all data from the database
 def db_select_all_notes(conn,table):
     conn.database = "db_notes"
     query = "SELECT * from "+ str(table)
@@ -56,6 +64,7 @@ def db_select_all_notes(conn,table):
     mycursor.execute(query)
     return mycursor.fetchall()
 
+# function to retrieve specific data from the database
 def db_select_specific_note(conn,table, chapter):
     conn.database = "db_notes"
     mycursor = conn.cursor()
@@ -66,7 +75,7 @@ def db_select_specific_note(conn,table, chapter):
 print(db_select_all_notes(conn,bookinput))
 print(db_select_specific_note(conn,bookinput,2))
 
-
+# function to delete notes in database
 def db_delete_note(conn,table, chapter):
     conn.database = "db_notes"
     mycursor = conn.cursor()
